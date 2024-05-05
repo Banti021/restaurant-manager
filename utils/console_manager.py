0cfb0784e3084ebb2858a0eb3f2ca7ad6d4fdd6c
Banti021 <igor.lasota21@gmail.com>
2024-05-05 15:25:34 +0200

    Added console manager

---
+++ utils/console_manager.py
@@ -0,0 +1,26 @@
+import os
+
+
+class ConsoleManager:
+    """Class to manage console input and output"""
+
+    @staticmethod
+    def display_menu(menu):
+        """Display menu options"""
+        for index, option in enumerate(menu, 1):
+            print(f"{index}. {option}")
+
+        choice = ConsoleManager.get_input("Enter your choice: ")
+        return int(choice)
+
+    @staticmethod
+    def get_input(message=""):
+        return input(message)
+
+    @staticmethod
+    def display_message(message):
+        print(message)
+
+    @staticmethod
+    def clear_screen():
+        os.system('cls' if os.name == 'nt' else 'clear')
