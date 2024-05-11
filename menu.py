39dfee4d9d1027a00953f9b53a854527711ce58a
Banti021 <igor.lasota21@gmail.com>
2024-05-11 22:54:13 +0200

    Refactored menu functionalities. Adjusted to ORM

--- menu.py
+++ menu.py
@@ -1,8 +1,7 @@
 import logging
 
-from utils.data_loader import DataLoader
-from models.dish import Dish
-from models.drink import Drink
+from services.dish_service import DishService
+from services.drink_service import DrinkService
 from utils.console_manager import ConsoleManager
 from utils.interactive_menu_handler import InteractiveMenuHandler
 from utils.menu_manager import MenuManager
@@ -18,22 +17,26 @@ class Menu:
             ("Usuń danie z menu", Menu.remove_dish_from_menu),
             ("Usuń napoj z menu", Menu.remove_drink_from_menu),
             ("Aktualizuj danie na menu", Menu.update_dish_on_menu),
+            ("Aktualizuj napój na menu", Menu.update_drink_on_menu),
             ("Ustaw danie dnia", Menu.set_dish_of_the_day),
-            ("Powrót", None)
+            ("Powrót", lambda: "back")
         ]
         menu_handler = InteractiveMenuHandler(options)
         menu_handler.run()
 
     @staticmethod
     def display_restaurant_menu():
-        dishes = DataLoader.load_items('data/dishes.json', Dish)
-        drinks = DataLoader.load_items('data/drinks.json', Drink)
+        dishes = DishService.get_all_dishes()
+        drinks = DrinkService.get_all_drinks()
 
         ConsoleManager.clear_screen()
         ConsoleManager.display_message("Aktualne Menu:")
 
+        ConsoleManager.display_message("Dania:")
         for dish in dishes:
             ConsoleManager.display_message(str(dish))
+
+        ConsoleManager.display_message("Napoje:")
         for drink in drinks:
             ConsoleManager.display_message(str(drink))
 
@@ -44,57 +47,79 @@ class Menu:
     def add_dish_to_menu():
         try:
             name, price, is_dish_of_the_day = MenuManager.get_dish_details()
-            dishes = DataLoader.load_items('data/dishes.json', Dish)
-            dish_id = DataLoader.get_next_id(dishes)
-            dish = Dish(dish_id, name, price, is_dish_of_the_day)
-            MenuManager.save_new_dish(dish)
-            logging.info(f"Dish added successfully: {name}")
+            DishService.create_dish(name, price, is_dish_of_the_day)
+            ConsoleManager.display_message("Danie zostało dodane do menu")
         except Exception as e:
-            logging.error(f"Failed to add dish: {e}")
-            ConsoleManager.display_message("Error adding dish.")
+            logging.exception(f"Error while adding dish to menu: {e}")
 
     @staticmethod
     def add_drink_to_menu():
-        name, price = MenuManager.get_drink_details()
-        drink_id = DataLoader.get_next_id('data/drinks.json')
-        drink = Drink(drink_id, name, price)
-        MenuManager.save_new_drink(drink)
+        try:
+            name, price, is_alcoholic, alcohol_content = MenuManager.get_drink_details()
+            DrinkService.create_drink(name, price, is_alcoholic, alcohol_content)
+            ConsoleManager.display_message("Napój został dodany do menu")
+        except Exception as e:
+            logging.exception(f"Error while adding drink to menu: {e}")
 
     @staticmethod
     def remove_dish_from_menu():
-        dishes = DataLoader.load_items('data/dishes.json', Dish)
-        for dish in dishes:
-            ConsoleManager.display_message(str(dish))
+        try:
+            dishes = DishService.get_all_dishes()
+            for dish in dishes:
+                ConsoleManager.display_message(str(dish))
 
-        dish_id = int(ConsoleManager.get_input("Podaj id dania do usunięcia: "))
-        MenuManager.delete_dish(dish_id)
+            dish_id = int(ConsoleManager.get_input("Podaj id dania do usunięcia: "))
+            DishService.delete_dish(dish_id)
+            ConsoleManager.display_message("Danie zostało usunięte z menu")
+        except Exception as e:
+            logging.exception(f"Error while removing dish from menu: {e}")
 
     @staticmethod
     def remove_drink_from_menu():
-        drinks = DataLoader.load_items('data/drinks.json', Drink)
-        for drink in drinks:
-            ConsoleManager.display_message(str(drink))
+        try:
+            drinks = DrinkService.get_all_drinks()
+            for drink in drinks:
+                ConsoleManager.display_message(str(drink))
 
-        drink_id = int(ConsoleManager.get_input("Podaj id napoju do usunięcia: "))
-        MenuManager.delete_drink(drink_id)
+            drink_id = int(ConsoleManager.get_input("Podaj id napoju do usunięcia: "))
+            DrinkService.delete_drink(drink_id)
+        except Exception as e:
+            logging.exception(f"Error while removing drink from menu: {e}")
 
     @staticmethod
     def update_dish_on_menu():
-        dishes = DataLoader.load_items('data/dishes.json', Dish)
-        for dish in dishes:
-            ConsoleManager.display_message(str(dish))
+        try:
+            dishes = DishService.get_all_dishes()
+            for dish in dishes:
+                ConsoleManager.display_message(str(dish))
 
-        dish_id = int(ConsoleManager.get_input("Podaj id dania do aktualizacji: "))
-        name, price, is_dish_of_the_day = MenuManager.get_dish_details()
-        new_dish = Dish(dish_id, name, price, is_dish_of_the_day)
-        MenuManager.update_dish(dish_id, new_dish)
+            dish_id = int(ConsoleManager.get_input("Podaj id dania do aktualizacji: "))
+            name, price, is_dish_of_the_day = MenuManager.get_dish_details()
+            DishService.update_dish(dish_id, name, price, is_dish_of_the_day)
+        except Exception as e:
+            logging.exception(f"Error while updating dish on menu: {e}")
+
+    @staticmethod
+    def update_drink_on_menu():
+        try:
+            drinks = DrinkService.get_all_drinks()
+            for drink in drinks:
+                ConsoleManager.display_message(str(drink))
+
+            drink_id = int(ConsoleManager.get_input("Podaj id napoju do aktualizacji: "))
+            name, price, is_alcoholic, alcohol_content = MenuManager.get_drink_details()
+            DrinkService.update_drink(drink_id, name, price, is_alcoholic, alcohol_content)
+        except Exception as e:
+            logging.exception(f"Error while updating drink on menu: {e}")
 
     @staticmethod
     def set_dish_of_the_day():
-        dishes = DataLoader.load_items('data/dishes.json', Dish)
-        for dish in dishes:
-            ConsoleManager.display_message(str(dish))
+        try:
+            dishes = DishService.get_all_dishes()
+            for dish in dishes:
+                ConsoleManager.display_message(str(dish))
 
-        dish_id = int(ConsoleManager.get_input("Podaj id dania dnia: "))
-        dish = next((dish for dish in dishes if dish.id == dish_id), None)
-        MenuManager.update_dish(dish_id, dish)
+            dish_id = int(ConsoleManager.get_input("Podaj id dania dnia: "))
+            DishService.set_dish_of_the_day(dish_id)
+        except Exception as e:
+            logging.exception(f"Error while setting dish of the day: {e}")
