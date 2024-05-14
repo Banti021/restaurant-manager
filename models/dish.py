2d28764a8d0a81cbe9373bffc08c5f54efe9a8e5
Banti021 <igor.lasota21@gmail.com>
2024-05-14 23:11:59 +0200

    Refactored models

--- models/dish.py
+++ models/dish.py
@@ -1,12 +1,14 @@
 from sqlalchemy import Column, Integer, String, Numeric, Boolean
+from sqlalchemy.orm import relationship
+
 from database.database import Base
 
 
 class Dish(Base):
     __tablename__ = "dishes"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
     price = Column(Numeric(6, 2), nullable=False)
     dish_of_the_day = Column(Boolean, default=False)
 
